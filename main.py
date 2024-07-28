53906b8596a6eff1d4c87896cfa18baad5c315c6
Ayoub Bouz <ayoub.bouz10@gmail.com>
2024-07-28 04:34:07 +0100

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from src.powerconsumptiontetouan import logger
+from powerconsumptiontetouan import logger
+from powerconsumptiontetouan.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome back a khayi !")
+# logger.info("Welcome back Bro !")
+
+
+STAGE_NAME = "Data Ingestion"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
