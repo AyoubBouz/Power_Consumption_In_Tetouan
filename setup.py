ee2d0438059184b20cc1a6bf12eb96178578c617
Ayoub Bouz <ayoub.bouz10@gmail.com>
2024-07-28 02:22:05 +0100

    req and setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.1.0"
+
+REPO_NAME = "Power_Consumption_In_Tetouan"
+AUTHOR_USER_NAME = "AyoubBouz"
+SRC_REPO = "powerconsumptiontetouan"
+AUTHOR_EMAIL = "contact@ayoubbouz.me"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
