2c6e8856842609d1ca8798c136957e92f3299df7
Ayoub Bouz <ayoub.bouz10@gmail.com>
2024-07-28 06:45:36 +0100

    data validation and schema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from powerconsumptiontetouan import logger
 from powerconsumptiontetouan.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
-
+from powerconsumptiontetouan.pipeline.stage_data_validation import DataValidationTrainingPipeline
 # logger.info("Welcome back Bro !")
 
 
@@ -10,6 +10,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/powerconsumptiontetouan/utils/common.py
+++ src/powerconsumptiontetouan/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.powerconsumptiontetouan import logger
+from powerconsumptiontetouan import logger
 import json
 import joblib
 from ensure import ensure_annotations
